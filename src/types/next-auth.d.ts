ff2d6a8501afe89b41f265ebc27c93de4f6fec52
Cristiano Amorim <cristianoretiro2003@gmail.com>
2023-04-04 20:16:46 -0300

    Create custom type to SessionUser

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,12 +1,13 @@
 import { type DefaultSession } from "next-auth";
 
+export type SessionUser = {
+  id: string;
+} & DefaultSession["user"];
 declare module "next-auth" {
   /**
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
   interface Session {
-    user?: {
-      id: string;
-    } & DefaultSession["user"];
+    user?: SessionUser;
   }
 }
